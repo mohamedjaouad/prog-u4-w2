c24cbdffd7b3ffcd0c88cc544d88858a52338034
mohamed jaouad <jaouadmohamed080@gmail.com>
2026-06-12 10:20:45 +0200

    create game class

--- src/main/java/MohamedJaouad/Games/Game.java
+++ src/main/java/MohamedJaouad/Games/Game.java
@@ -3,15 +3,37 @@ package MohamedJaouad.Games;
 import java.time.LocalDate;
 
 public abstract class Game {
-    private int countID=1;
+
+    private int id;
     private String title;
     private LocalDate annoPub;
     private double price;
-    public Game( String title,LocalDate annoPub,double price){
 
+    private int countID=1;
+
+    public Game( String title,LocalDate annoPub,double price){
+        this.id=countID++;
         this.title=title;
         this.annoPub=annoPub;
         this.price=price;
     }
+    public int getId() { return id; }
+    public String getTitle() { return title; }
+    public LocalDate getAnnoPub() { return annoPub; }
+    public double getPrice() { return price; }
 
+    public void setTitle(String title) { this.title = title; }
+    public void setAnnoPub(LocalDate annoPub) { this.annoPub = annoPub; }
+    public void setPrice(double price) { this.price = price; }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", annoPub=" + annoPub +
+                ", price=" + price +
+                ", countID=" + countID +
+                '}';
+    }
 }
